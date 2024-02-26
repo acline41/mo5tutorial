c3c099a054227632b74fd29f5efe2522dda1fcab
acline41 <156849994+acline41@users.noreply.github.com>
2024-02-26 20:33:32 +0000

    changed

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
+//connect to mongodb
+const dbURI= 'mongodb+srv://alexcline:scammer12@cluster0.vsbmtf0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
+.then((result) => app.listen(3000))
+.catch((err) => console.log(err));
 
 // express app
 const app = express();
-
-// listen for requests
-app.listen(3000);
 
 // register view engine
 app.set('view engine', 'ejs');
 
 // middleware & static files
 app.use(express.static('public'));
+app.use(morgan('dev'))
 
 app.use((req, res, next) => {
   console.log('new request made:');
@@ -21,12 +26,37 @@ app.use((req, res, next) => {
   next();
 });
 
+app.get('/add-blog', (req, res) => {
+  const blog = new Blog({
+    title: 'new blog',
+    snippet:'about my new blog',
+    body: 'more about my new blog'
+  });
+  blog.save()
+  .then((result)=> {
+    res.send(result)
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+});
+app.get('/all-blogs', (req, res) =>{
+  blog.find()
+  .then((result)=> {
+    res.send(result);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+})
+///app.get('/single-blog',(req, res)=> {
+  
 app.use((req, res, next) => {
   console.log('in the next middleware');
   next();
 });
 
-app.use(morgan('dev'));
+
 
 app.use((req, res, next) => {
   res.locals.path = req.path;
